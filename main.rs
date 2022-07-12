4fb76f6b9c381203de00dd871b47c83e923231b2
mrodz <79176075+mrodz@users.noreply.github.com>
2022-07-12 15:53:42 -0700

    documentation

--- main.rs
+++ main.rs
@@ -1,25 +1,25 @@
+/// Entrypoint to the program
 fn main() {
     let mut game: Game = Game::new();
-    game.start();
     game.start();
 }
 
 /// ### Abstraction for a Game of TicTacToe.
-/// ```
+/// ```no_run
 /// let g: Game = Game::new();
 /// g::start();
 /// ```
 /// to start a round.
 #[derive(Debug)]
-struct Game {
+pub struct Game {
     grid: Grid,
-    count: u32,
+    pub count: u32,
 }
 
 /// ### Maximum turns allowed in a game.
 /// Since a game's `count` starts at zero, this bound allows 
 /// nine total moves for nine total slots.
-const MAX_TURNS: u32 = 8;
+pub const MAX_TURNS: u32 = 8;
 
 impl Game {
     /// ### Start the game.
@@ -31,7 +31,7 @@ impl Game {
         tutorial();
         loop {
             if self.step() {
-                break;
+                break; // game is over
             }
         }
         self.clean_up();
@@ -50,7 +50,7 @@ impl Game {
     /// - Applies end-of-game logic if applicable.
     /// 
     /// Returns a boolean value: `true` if the game is over, otherwise `false`.
-    pub fn step(&mut self) -> bool {
+    fn step(&mut self) -> bool {
         let symbol: char = turn(self.count);
         self.grid.print();
 
@@ -60,7 +60,7 @@ impl Game {
             Ok(n) => n,
             Err(msg) => {
                 println!("{}\nPlease try again...\n", msg);
-                return false;
+                return false; // void this step
             }
         };
 
@@ -78,14 +78,14 @@ impl Game {
     /// 
     /// Will increment `count` if the game is not over.
     /// If the game has ended, will also print a win message.
-    pub fn check_end(&mut self) -> bool {
+    fn check_end(&mut self) -> bool {
         match self.grid.get_winner(self.count) {
             GameStates::StillPlaying => {
                 self.increment_count();
                 return false;
             }
             win_state => {
-                println!("{}", GameStates::win_msg(win_state));
+                println!("{}", win_state.win_msg());
             }
         }
         true
@@ -116,7 +116,7 @@ fn tutorial() -> () {
 }
 
 /// Shorthand for getting the current player
-fn turn(count: u32) -> char {
+const fn turn(count: u32) -> char {
     if count % 2 == 0 {
         'X'
     } else {
@@ -139,7 +139,7 @@ pub fn prompt_usize(maybe_text: Option<&str>) -> Result<usize, String> {
 /// 
 /// Resembles python's `input()` function.
 /// Usage:
-/// ```
+/// ```no_run
 /// let name = prompt(Some("Enter your name: "));
 /// println!("Hello {}!", name);
 /// ```
@@ -150,7 +150,7 @@ pub fn prompt(maybe_text: Option<&str>) -> String {
     if let Some(text) = maybe_text {
         print!("{}", text);
     }
-
+    
     let _ = stdout().flush();
 
     stdin()
@@ -171,6 +171,10 @@ pub fn prompt(maybe_text: Option<&str>) -> String {
 }
 
 /// ### Abstraction to get user input.
+/// 
+/// Returns either an `Ok(usize)` with the placement or an `Err(str)` with the error message.
+/// 
+/// Will only return `Ok(usize)` if the user input is a number and a valid placement.
 fn read_placement(target: &Grid) -> Result<usize, &str> {
     match prompt_usize(Some("Enter cell number: ")) {
         Ok(n) => target.valid_plot(n),
@@ -178,12 +182,22 @@ fn read_placement(target: &Grid) -> Result<usize, &str> {
     }
 }
 
-pub fn grid_mapping_to_indexes(mapping: usize) -> [usize; 2] {
+/// Converts a plotting to an actual matrix index. 
+/// 
+/// ### Examples:
+/// ```no_run
+/// assert_eq!([2, 2], grid_mapping_to_indexes(5));
+/// assert_eq!([0, 0], grid_mapping_to_indexes(1));
+/// assert_eq!([1, 2], grid_mapping_to_indexes(6));
+/// ```
+const fn grid_mapping_to_indexes(mapping: usize) -> [usize; 2] {
     let m = mapping - 1;
 
     [m / 3, m % 3]
 }
 
+/// Used to represent the current state of the game.
+/// * `Win(char)` will contain the character of the winner.
 enum GameStates {
     Win(char),
     Tie,
@@ -191,8 +205,11 @@ enum GameStates {
 }
 
 impl GameStates {
-    fn win_msg(state: GameStates) -> String {
-        match state {
+    /// Function to build an end-of-game message, given an end of game.
+    /// 
+    /// Will panic if caller is `GameStates::StillPlaying`
+    fn win_msg(&self) -> String {
+        match self {
             GameStates::StillPlaying => panic!("active game is not fit for a win message"),
             GameStates::Tie => String::from("The game is a tie!"),
             GameStates::Win(c) => format!("{} wins!", c),
@@ -200,32 +217,54 @@ impl GameStates {
     }
 }
 
+/// Abstraction for a character matrix.
 #[derive(Debug)]
 struct Grid {
+    /// contents of the board 
     grid: [[char; 3]; 3],
 }
 
 impl Grid {
-    fn _at_indexes(&self, x: usize, y: usize) -> char {
+    /// Accessor function
+    /// ### Arguments
+    /// * `x` - grid **ROW**
+    /// * `y` - grid **COLUMN**
+    fn at_indexes(&self, x: usize, y: usize) -> char {
         self.grid[x][y]
     }
 
+    /// Abstraction for `at_indexes()`
+    /// ### Arguments
+    /// * `plotting` - number in range of (1..=9) mapped to a slot on the grid.
     pub fn at(&self, plotting: usize) -> char {
         let mapped: [usize; 2] = grid_mapping_to_indexes(plotting);
-        self._at_indexes(mapped[0], mapped[1])
+        self.at_indexes(mapped[0], mapped[1])
     }
 
-    fn _plot_at_indexes(&mut self, x: usize, y: usize, c: char) -> char {
+    /// Setter function
+    /// ### Arguments
+    /// * `x` - grid **ROW**
+    /// * `y` - grid **COLUMN**
+    /// * `c` - new character
+    fn plot_at_indexes(&mut self, x: usize, y: usize, c: char) -> char {
         let old = self.grid[x][y];
         self.grid[x][y] = c;
         old
     }
 
+    /// Abstraction for `plot_at_indexes()`
+    /// ### Arguments
+    /// * `plotting` - number in range of (1..=9) mapped to a slot on the grid.
+    /// * `c` - new character
     pub fn plot(&mut self, plotting: usize, c: char) -> char {
         let mapped: [usize; 2] = grid_mapping_to_indexes(plotting);
-        self._plot_at_indexes(mapped[0], mapped[1], c)
+        self.plot_at_indexes(mapped[0], mapped[1], c)
     }
 
+    /// Returns whether or not a plot is valid.
+    /// 
+    /// A plot is valid if `num` fits the range of 1-9 (inclusive)
+    /// and the requested slot is empty.
     pub fn valid_plot(&self, num: usize) -> Result<usize, &str> {
         if let 1..=9 = num {
             if self.at(num) == '_' {
@@ -238,6 +277,7 @@ impl Grid {
         }
     }
 
+    /// Decorative way to print the grid
     pub fn print(&self) -> () {
         println!("---------");
         for g_r in self.grid {
@@ -248,6 +288,7 @@ impl Grid {
             println!("|");
         }
         println!("---------");
+        // let _ = std::io::stdout().flush();
     }
 
     fn get_winner(&self, turns: u32) -> GameStates {
@@ -287,6 +328,18 @@ impl Grid {
         }
     }
 
+    /// ### Constructor
+    /// Arguments:
+    /// * `gen` - optional parameter; a function that outputs a character when given an index.
+    /// 
+    /// Usage:
+    /// ```no_run
+    /// // grid filled with empty slots ('_')
+    /// let grid_1 = Grid::new(None);
+    /// 
+    /// // grid filled with numbers
+    /// let grid_2 = Grid::new(Some(|i| char::from_digit(i as u32, 10).unwrap()))
+    /// ```
     pub fn new(gen: Option<fn(usize) -> char>) -> Self {
         let mut grid: [[char; 3]; 3] = [['_'; 3]; 3];
 
